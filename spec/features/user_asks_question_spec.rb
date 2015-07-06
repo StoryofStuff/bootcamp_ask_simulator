8a486fb3403ba447bad1643ffeaeb64bb73a536e
Josh Morrow <josh@jcmorrow.com>
2015-07-06 14:56:36 -0700

    Changes to readme and fixing an error in the specs

--- spec/features/user_asks_question_spec.rb
+++ spec/features/user_asks_question_spec.rb
@@ -5,6 +5,6 @@ feature "User asks a question" do
     visit root_path
     fill_in :question_text, with: "Will you marry me?"
     click_on 'Ask'
-    expect(page).to have_css('.question'), text: "Will you marry me?"
+    expect(page).to have_css('.question', text: "Will you marry me?")
   end
 end
